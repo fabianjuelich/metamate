33103f48a27aeb332fa982cab7ae31a4915be98b
Fabian <fabian@debian.fritz.box>
2022-11-25 23:55:32 +0100

    Improve path entry pathholder and change color theme

--- metamate.py
+++ metamate.py
@@ -1,12 +1,15 @@
 # Python 3.9.2
 
-from enum import Enum
+import os
 from glob import glob
-from os import stat, path, rename
+from enum import Enum
 from time import strftime, localtime
 import tkinter as tk
 from tkinter import filedialog
 import customtkinter as ctk
+
+# root directory for placeholder text and safety
+rootDir = os.path.abspath(os.sep)
 
 class Tag(Enum):
     CREATION = 9
@@ -26,7 +29,7 @@ optionsSep = dict(zip(["_", "-", ".", " "], list(Sep)))
 
 # appearance and color theme
 ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("green")
+ctk.set_default_color_theme("sweetkind")
 
 class App(ctk.CTk):
 
@@ -56,7 +59,7 @@ class App(ctk.CTk):
         # entry path
         self.path = ctk.CTkEntry(
             width=600, height=40, corner_radius=20, textvariable=self.variablePth)
-        self.path.insert(0, "/")
+        self.path.insert(0, rootDir)
         self.path.grid(row=2, column=0, columnspan=2, padx=50)
 
         # button browse
@@ -66,13 +69,13 @@ class App(ctk.CTk):
 
         # option-menu tag
         self.tag = ctk.CTkOptionMenu(
-            width=280, height=40, button_color="#11B384", button_hover_color="#0D8A66", corner_radius=20, variable=self.variableTag, values=list(optionsTag.keys()))
+            width=280, height=40, button_hover_color=("#8593d6", "#171926"), corner_radius=20, variable=self.variableTag, values=list(optionsTag.keys()))
         self.tag.set(list(optionsTag)[0])
         self.tag.grid(row=4, column=0, padx=20, sticky="e")
 
         # option-menu seperator
         self.sep = ctk.CTkOptionMenu(
-            width=280, height=40, button_color="#11B384", button_hover_color="#0D8A66", corner_radius=20, variable=self.variableSep, values=list(optionsSep.keys()))
+            width=280, height=40, button_hover_color=("#8593d6", "#171926"), corner_radius=20, variable=self.variableSep, values=list(optionsSep.keys()))
         self.sep.set(list(optionsSep)[0])
         self.sep.grid(row=4, column=1, padx=20, sticky="w")
 
@@ -90,7 +93,10 @@ class App(ctk.CTk):
     
     # generate information and launch renaming
     def renameDirectory(self):
-        self.renameFiles(files=glob(f"{self.variablePth.get()}/*"), tag=optionsTag[self.variableTag.get()], sep=optionsSep[self.variableSep.get()])
+        if self.variablePth.get() != rootDir:
+            self.renameFiles(files=glob(f"{self.variablePth.get()}/*"), tag=optionsTag[self.variableTag.get()], sep=optionsSep[self.variableSep.get()])
+        else:
+            pass # warning icon
 
     # renaming
     def renameFiles(self, files: list, tag: Tag, sep: Sep):
@@ -101,9 +107,9 @@ class App(ctk.CTk):
             except:
                 print("File not found")
 
-            old = path.realpath(file)
-            root, ext = path.splitext(old)
-            meta = stat(file)
+            old = os.path.realpath(file)
+            root, ext = os.path.splitext(old)
+            meta = os.stat(file)
 
             if(tag == tag.SIZE):
                 appendix = f"{meta[tag.value]}Byte"
@@ -117,7 +123,7 @@ class App(ctk.CTk):
 
             new = f"{root}{sep.value}{appendix}{ext}"
             # maintenance
-            # rename(old, new)
+            # os.rename(old, new)
             renamedFiles.append([old, new])
             print(f"{old}\n{new}\n{'_' * 100}")
 
